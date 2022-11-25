0df0d528a96dbdb234fe69ca53834262818594ae
pedroripper <pedros.ripper@gmail.com>
2022-11-25 17:32:15 -0300

    Minor changes

--- plot.py
+++ plot.py
@@ -6,6 +6,8 @@ toqubo_data = read_csv("ToQUBO/tsp_ToQUBO.csv")
 pyqubo_current_data = read_csv("pyqubo/tsp_pyqubo.csv")
 pyqubo_040_data = read_csv("pyqubo/tsp_pyqubo_0_4_0.csv")
 
+plt.figure(figsize = (10,10))
+
 plt.style.use(['science','no-latex'])
 
 plt.plot(toqubo_data["n_var"], toqubo_data["time"], label = "ToQUBO 0.1.3", marker='o')
@@ -13,14 +15,13 @@ plt.plot(pyqubo_current_data["n_var"], pyqubo_current_data["time"], label = "PyQ
 plt.plot(pyqubo_040_data["n_var"], pyqubo_040_data["time"], label = "PyQUBO 0.4.0", marker='o')
 
 
+
 plt.xscale('symlog')
 plt.yscale('symlog')
 plt.xlabel("#variables")
 plt.ylabel("Execution Time(sec)")
 plt.grid(True)
-
 plt.legend()
-
 plt.savefig('benchmark.png')
 
 plt.show()
