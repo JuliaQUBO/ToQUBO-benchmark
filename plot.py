cb9d5c22ec7e05954649c4602cdb0530b05757e8
pedroripper <pedros.ripper@gmail.com>
2022-11-25 17:29:02 -0300

    Change plot

--- plot.py
+++ plot.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 from pandas import read_csv
+import scienceplots
 
 toqubo_data = read_csv("ToQUBO/tsp_ToQUBO.csv")
 pyqubo_current_data = read_csv("pyqubo/tsp_pyqubo.csv")
 pyqubo_040_data = read_csv("pyqubo/tsp_pyqubo_0_4_0.csv")
 
+plt.style.use(['science','no-latex'])
 
 plt.plot(toqubo_data["n_var"], toqubo_data["time"], label = "ToQUBO 0.1.3", marker='o')
 plt.plot(pyqubo_current_data["n_var"], pyqubo_current_data["time"], label = "PyQUBO 1.3.1", marker='o')
 plt.plot(pyqubo_040_data["n_var"], pyqubo_040_data["time"], label = "PyQUBO 0.4.0", marker='o')
-
 
 
 plt.xscale('symlog')
