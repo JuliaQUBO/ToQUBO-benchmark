402f6fb3e808594b5e0ac29150f942e23b83a501
Pedro Maciel Xavier <pedroxavier@psr-inc.com>
2022-12-05 22:04:28 -0300

    Update sysimage precompilation

--- benchmark/ToQUBO/create_sysimage.jl
+++ benchmark/ToQUBO/create_sysimage.jl
@@ -1,6 +1,6 @@
 using PackageCompiler, Libdl
 PackageCompiler.create_sysimage(
-    ["JuMP", "ToQUBO", "CSV","Profile","ProfileSVG"],
-    sysimage_path = joinpath(@__DIR__, "sysimage." * Libdl.dlext),
-    precompile_execution_file = [ joinpath(@__DIR__, "precompile.jl")],
+    ["JuMP", "ToQUBO", "Anneal", "CSV", "Profile", "ProfileSVG"],
+    sysimage_path = joinpath(@__DIR__, "sysimage.$(Libdl.dlext)"),
+    precompile_execution_file = [ joinpath(@__DIR__, "precompile.jl") ],
 )
--- benchmark/ToQUBO/precompile.jl
+++ benchmark/ToQUBO/precompile.jl
@@ -3,27 +3,17 @@ using JuMP
 using ToQUBO
 using Anneal
 
-function tsp(n)
-    t₀ = @timed begin    
-        model = Model(ToQUBO.Optimizer)
-    
-        @variable(model, x[1:n, 1:n], Bin, Symmetric)
-    
-        @constraint(model, [i in 1:n], sum(x[i,:]) == 1)
-        @constraint(model, [j in 1:n], sum(x[:,j]) == 1)
-    
-        D = fill(10, (n,n))
-    
-        @objective(model, Min, sum([D[i,j] * x[i,k] * x[j, (k % n)+1] for i = 1:n , j = 1:n, k = 1:n]))
-    
-        optimize!(model)
-    end
-    
-    t₁ = @timed begin
-        qubo_model = ToQUBO.toqubo(JuMP.backend(model).model_cache)
-        Q, α, β    = ToQUBO.qubo(qubo_model)
-    end    
-end
+n     = 5
+D     = fill(10, (n,n))
+model = Model(ToQUBO.Optimizer)
 
-tsp(5)
-tsp(10)
+@variable(model, x[1:n, 1:n], Bin, Symmetric)
+
+@constraint(model, [i in 1:n], sum(x[i,:]) == 1)
+@constraint(model, [j in 1:n], sum(x[:,j]) == 1)
+
+@objective(model, Min, sum([D[i,j] * x[i,k] * x[j, (k % n)+1] for i = 1:n , j = 1:n, k = 1:n]))
+
+optimize!(model)
+
+Q, α, β = Anneal.qubo(model)
