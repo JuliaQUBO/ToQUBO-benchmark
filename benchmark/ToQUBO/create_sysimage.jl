41042d8e7f009f0da88e11b29e87b158c2df04a7
Pedro Ripper <pedros.ripper@gmail.com>
2022-12-08 08:24:27 -0300

    Small fix

--- benchmark/ToQUBO/create_sysimage.jl
+++ benchmark/ToQUBO/create_sysimage.jl
@@ -2,5 +2,5 @@ using PackageCompiler, Libdl
 PackageCompiler.create_sysimage(
     ["JuMP", "ToQUBO"],
     sysimage_path = joinpath(@__DIR__, "sysimage.$(Libdl.dlext)"),
-    precompile_execution_file = [ joinpath(@__DIR__, "tsp.jl") ],
+    precompile_execution_file = [ joinpath(@__DIR__, "precompile.jl") ],
 )
