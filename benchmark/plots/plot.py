909a77a607433caecb1faeb4f4446b113d5cf4bd
Pedro Maciel Xavier <pedroxavier@psr-inc.com>
2022-12-08 23:20:09 -0300

    Update Actions

--- benchmark/plots/plot.py
+++ benchmark/plots/plot.py
@@ -1,25 +1,50 @@
 import matplotlib.pyplot as plt
-from pandas import read_csv
 import scienceplots
+from pandas import read_csv
 from pathlib import Path
 
 BASE_PATH = Path.cwd().joinpath("benchmark")
 
 def plot_benchmark():
-    toqubo_data         = read_csv(BASE_PATH.joinpath("ToQUBO", "tsp_ToQUBO.csv"))
-    pyqubo_current_data = read_csv(BASE_PATH.joinpath("pyqubo", "tsp_pyqubo.csv"))
+    toqubo_data         = read_csv(BASE_PATH.joinpath("ToQUBO"    , "tsp_ToQUBO.csv"))
+    pyqubo_current_data = read_csv(BASE_PATH.joinpath("pyqubo"    , "tsp_pyqubo.csv"))
     pyqubo_040_data     = read_csv(BASE_PATH.joinpath("pyqubo_040", "tsp_pyqubo_040.csv"))
-    qubovert_data = read_csv(BASE_PATH.joinpath("qubovert", "tsp_qubovert.csv"))
+    qubovert_data       = read_csv(BASE_PATH.joinpath("qubovert"  , "tsp_qubovert.csv"))
 
     plt.figure(figsize = (5,4))
 
     plt.style.use(['science','no-latex'])
 
-    plt.plot(toqubo_data["n_var"], toqubo_data["time"], label = "ToQUBO 0.1.3", marker='o')
-    plt.plot(pyqubo_current_data["n_var"], pyqubo_current_data["time"], label = "PyQUBO 1.3.1", marker='o')
-    plt.plot(pyqubo_040_data["n_var"], pyqubo_040_data["time"], label = "PyQUBO 0.4.0", marker='o')
-    plt.plot(qubovert_data["n_var"], qubovert_data["time"], label = "qubovert 1.2.5", marker='o')
-    plt.plot(toqubo_data["n_var"], toqubo_data["toqubo_time"], label = "ToQUBO* 0.1.3", marker='o')
+    plt.plot(
+        pyqubo_040_data["n_var"],
+        pyqubo_040_data["time"],
+        label = "PyQUBO 0.4.0",
+        marker='o'
+    )
+    plt.plot(
+        pyqubo_current_data["n_var"],
+        pyqubo_current_data["time"],
+        label = "PyQUBO 1.3.1",
+        marker='o'
+    )
+    plt.plot(
+        toqubo_data["n_var"],
+        toqubo_data["time"],
+        label = "ToQUBO 0.1.3",
+        marker='o'
+    )
+    plt.plot(
+        toqubo_data["n_var"],
+        toqubo_data["toqubo_time"],
+        label = "ToQUBO* 0.1.3",
+        marker='o'
+    )
+    plt.plot(
+        qubovert_data["n_var"],
+        qubovert_data["time"],
+        label = "qubovert 1.2.5",
+        marker='o'
+    )
 
     plt.xscale('symlog')
     plt.yscale('symlog')
@@ -38,8 +63,8 @@ def plot_toqubo():
 
     plt.style.use(['science','no-latex'])
 
-    plt.plot(toqubo_data["n_var"], toqubo_data["toqubo_time"], label = "ToQUBO", marker='o')
-    plt.plot(toqubo_data["n_var"], toqubo_data["jump_time"], label = "JuMP", marker='o')
+    plt.plot(toqubo_data["n_var"], toqubo_data["toqubo_time"], label = "ToQUBO", marker='D')
+    plt.plot(toqubo_data["n_var"], toqubo_data["jump_time"]  , label = "JuMP"  , marker='D')
     plt.xscale('symlog')
     plt.yscale('symlog')
     plt.xlabel("#variables")
